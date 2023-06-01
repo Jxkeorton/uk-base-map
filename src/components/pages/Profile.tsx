6cc54c1484825512d95bb8c75b07f77210a59acc
Jxkeorton <jakeorton99@gmail.com>
2023-06-01 09:58:15 +0100

    added logout function to profile page also displays user name when logged in

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -1,17 +1,53 @@
-import { getAuth, User as FirebaseUser } from 'firebase/auth'
+import { getAuth, User as FirebaseUser,  onAuthStateChanged} from 'firebase/auth'
 import { useState, useEffect } from 'react'
+import { useNavigate, Link} from 'react-router-dom'
+
+interface CustomUser extends FirebaseUser {
+  displayName: string | null;
+  email: string | null;
+}
 
 function Profile() {
-    const [user, setUser] = useState<FirebaseUser | null>(null)
+  const auth = getAuth();
+  const [formData, setFormData] = useState<CustomUser | null>(null);
 
-    const auth = getAuth()
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setFormData({
+          displayName: user.displayName ?? null,
+          email: user.email ?? null,
+        } as CustomUser | null);
+      }
+    });
 
-    useEffect(() => {
-        setUser(auth.currentUser)
-    },[])
+    return () => unsubscribe();
+  }, [auth]);
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  } 
 
 
-  return user ? <h1>{user.displayName}</h1> : <div>Not logged in</div>
+  return (
+    <div className='profile' >
+      <header className="profile-header">
+        <p className='page-header'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout} >
+          Logout
+        </button>
+      </header>
+      {formData && (
+        <div className="profile-info">
+          <p>Display Name: {formData.displayName}</p>
+          <p>Email: {formData.email}</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Profile
