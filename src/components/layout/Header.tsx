f01c7d055dd8fe4529fa9f29a36281eb6567733d
Jxkeorton <jakeorton99@gmail.com>
2023-06-30 12:05:18 +0100

    new header, better for phone screens

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,40 @@
 import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faSearch, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../../assets/png/MMB small logo.png'
+import {useRef, useState} from 'react'
 
 const Header = () => {
   const navigate = useNavigate()
+  const navRef = useRef<HTMLElement>(null);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const showNavbar = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const handleNavigate = (route: string) => {
+    navigate(route);
+    showNavbar();
+  };
 
   return (
-    <nav className="header" >
-        <ul className="h-ul" >
-            <li className='h-li'>
-              <a href='https://www.mountainmanbase.com/'  ><img src={Logo} alt='mountain man base logo' className='header-img'/></a>
-            </li>
-            <li className="h-li" onClick={() => navigate('/')} >
-              <p className="h-a" >UK BASE Map</p>
-            </li>
-            <li className='h-li' onClick={() => navigate('/search-locations')} >
-              <p className="h-a">Search</p>
-            </li>
-            <li className="profile-link" onClick={() => navigate('/profile')} >
-              <p className="h-a" >Profile</p>
-            </li>
-        </ul>
-    </nav>
+    <header>
+      <h3><a href='https://www.mountainmanbase.com/'  ><img src={Logo} alt='mountain man base logo' className='header-img'/></a></h3>
+      <nav className={isNavOpen ? 'responsive_nav' : ''} ref={navRef} >
+          <p onClick={() => handleNavigate('/')} >Map</p>
+          <p onClick={() => handleNavigate('/search-locations')} ><FontAwesomeIcon icon={faSearch} />
+              {' '}Search</p>
+          <p onClick={() => handleNavigate('/profile')}><FontAwesomeIcon icon={faUser} />
+              {' '}Profile</p>
+          <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+          <FontAwesomeIcon icon={faTimes} />
+          </button>
+      </nav>
+      <button className='nav-btn' onClick={showNavbar} >
+      <FontAwesomeIcon icon={faBars} />
+      </button>
+    </header>
   )
 }
 
