56271f20f75ba65466c38626305990d497412c32
Jxkeorton <jakeorton99@gmail.com>
2023-07-01 12:50:05 +0100

    improving header and footer components making code cleaner and easier to update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,46 +1,70 @@
-import Logo from '../../assets/png/MMB small logo.png'
+import Logo from '../../assets/png/MMB small logo.png';
 
 const Footer = () => {
+  const hoursOfOperation = [
+    'Mon-Thu: 9AM to 8PM',
+    'Friday: 9AM to 5PM',
+    'Sat-Sun: Jumping!'
+  ];
+
+  const contactEmail = 'info@mountainmanbase.com';
+
+  const menuLinks = [
+    { label: 'Home', url: 'https://www.mountainmanbase.com/' },
+    { label: 'Learn to Base', url: 'https://www.mountainmanbase.com/learn-to-base' },
+    { label: 'Media', url: 'https://www.mountainmanbase.com/media' },
+    { label: 'Events', url: 'https://www.mountainmanbase.com/high-performance' }
+  ];
+
   return (
-    <div className="footer">
-    <div className="contain">
+    <footer className="footer">
+      <div className="contain">
         <div className="col">
-        <img src={Logo} alt='mountain man base logo' className='footer-img'/>
-        <ul>
+          <img src={Logo} alt='mountain man base logo' className='footer-img' />
+          <ul>
             <li>© 2023 by Apex</li>
-        </ul>
+          </ul>
         </div>
 
         <div className="col">
-        <h1>Hours of operation</h1>
-        <ul>
-            <li>Mon-Thu: 9AM to 8PM</li>
-            <li>Friday: 9AM to 5PM</li>
-            <li>Sat-Sun: Jumping!</li>
-        </ul>
-        </div>
-        
-        <div className="col">
-        <h1>Contact us</h1>
-        <ul>
-            <li><a className="f-a" href="mailto:info@mountainmanbase.com" >info@mountainmanbase.com</a></li>
-        </ul>
+          <h1>Hours of operation</h1>
+          <ul>
+            {hoursOfOperation.map((hours, index) => (
+              <li key={index}>{hours}</li>
+            ))}
+          </ul>
         </div>
 
         <div className="col">
-            <h1>Menu</h1>
+          <h1>Contact us</h1>
+          <ul>
+            <li>
+              <a className="f-a" href={`mailto:${contactEmail}`}>
+                {contactEmail}
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col">
+          <h1>Menu</h1>
+          <nav>
             <ul>
-                <li><a className="f-a" href="https://www.mountainmanbase.com/">Home</a></li>
-                <li><a className="f-a" href="https://www.mountainmanbase.com/learn-to-base" >Learn to Base</a></li>
-                <li><a className="f-a" href="https://www.mountainmanbase.com/media" >Media</a></li>
-                <li><a className="f-a" href="https://www.mountainmanbase.com/high-performance" >Events</a></li>
+              {menuLinks.map((link, index) => (
+                <li key={index}>
+                  <a className="f-a" href={link.url} aria-label={link.label}>
+                    {link.label}
+                  </a>
+                </li>
+              ))}
             </ul>
+          </nav>
         </div>
 
         <div className="clearfix"></div>
-    </div>
-  </div>
-  )
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
-import { useNavigate } from 'react-router-dom'
+import { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faSearch, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import Logo from '../../assets/png/MMB small logo.png'
-import {useRef, useState} from 'react'
+import Logo from '../../assets/png/MMB small logo.png';
 
 const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const navRef = useRef<HTMLElement>(null);
   const [isNavOpen, setIsNavOpen] = useState(false);
 
@@ -18,24 +18,34 @@ const Header = () => {
     showNavbar();
   };
 
+  const navigationItems = [
+    { label: 'Map', route: '/' },
+    { label: 'Search', route: '/search-locations', icon: faSearch },
+    { label: 'Profile', route: '/profile', icon: faUser },
+  ];
+
   return (
     <header>
-      <h3><a href='https://www.mountainmanbase.com/'  ><img src={Logo} alt='mountain man base logo' className='header-img'/></a></h3>
-      <nav className={isNavOpen ? 'responsive_nav' : ''} ref={navRef} >
-          <p onClick={() => handleNavigate('/')} >Map</p>
-          <p onClick={() => handleNavigate('/search-locations')} ><FontAwesomeIcon icon={faSearch} />
-              {' '}Search</p>
-          <p onClick={() => handleNavigate('/profile')}><FontAwesomeIcon icon={faUser} />
-              {' '}Profile</p>
-          <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+      <h3>
+        <Link to="/">
+          <img src={Logo} alt="mountain man base logo" className="header-img" />
+        </Link>
+      </h3>
+      <nav className={isNavOpen ? 'responsive_nav' : ''} ref={navRef}>
+        {navigationItems.map((item, index) => (
+          <p key={index} onClick={() => handleNavigate(item.route)}>
+            {item.icon && <FontAwesomeIcon icon={item.icon} />} {item.label}
+          </p>
+        ))}
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FontAwesomeIcon icon={faTimes} />
-          </button>
+        </button>
       </nav>
-      <button className='nav-btn' onClick={showNavbar} >
-      <FontAwesomeIcon icon={faBars} />
+      <button className="nav-btn" onClick={showNavbar}>
+        <FontAwesomeIcon icon={faBars} />
       </button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
